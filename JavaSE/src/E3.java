89a63c83fad53e73d8ac5f62e376ebc40d4093b3
yaolong <398551223@qq.com>
2015-10-18 16:43:14 +0800

    Added some Java files

---
+++ JavaSE/src/E3.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2015/10/18.
+ */
+//打印出所有的’水仙花数’，所谓’水仙花数’是指一个三位数，其各位数字立方和等于该数本身。例如：153是一个’水仙花数’，因为153=1的三次方＋5的三次方＋3的三次方
+public class E3 {
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ JavaSE/src/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2015/10/18.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("2");
+    }
+}
