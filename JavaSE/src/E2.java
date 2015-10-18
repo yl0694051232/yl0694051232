18b94e66638115fbbbe2bb3bce0d52b10b83ebd9
yaolong <398551223@qq.com>
2015-10-18 16:52:41 +0800

    Added some Java files

--- JavaSE/src/E2.java
+++ JavaSE/src/E2.java
@@ -7,14 +7,17 @@
 public class E2 {
     public static void main(String[] args) {
         for (int i = 101; i <200 ; i++) {
+            boolean b = true;
             for (int j = 2; j < i; j++) {
                 if (i%j==0){
+                    b = false;
                     break;
                 }
-                if(i-j==1) {
-                    System.out.print(i+"\t");
-                }
+            }
+            if(b){
+                System.out.print(i+"\t");
             }
         }
+
     }
 }
