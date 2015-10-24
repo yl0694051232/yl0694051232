41f70c6bca1273c4b768a66bac9286ac39caf8c9
Administrator <398551223@qq.com>
2015-10-24 16:57:17 +0800

    update

--- JavaSE/src/Chinese.java
+++ JavaSE/src/Chinese.java
@@ -12,11 +12,21 @@ public class Chinese extends Human {
        return "learning KongFu.......";
     }
 
+    String sing() {
+        System.out.println("hello world!");
+        super.sing();
+        return "ad";
+
+    }
     public static void main(String[] args) {
         Chinese chinese = new Chinese("yellow");
         System.out.println(chinese.work(1,"abd"));
         System.out.println(chinese.skinColor);
         System.out.println(chinese.StudyKongfu());
+        String z = chinese.sing();
+        System.out.println(z);
+        //String f = super.sing();
+
     }
 
 }
